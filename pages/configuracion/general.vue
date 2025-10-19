354374dd4ef176827fb4b7e6369d3c9d87404401
alejandro ocampo <alejandro.ocampo@sumimedical.com>
2025-10-19 18:17:32 -0500

    Actualizacion configuracion

--- pages/configuracion/general.vue
+++ pages/configuracion/general.vue
@@ -3,7 +3,7 @@
     <v-card flat tile>
       <v-card-text class="mt-2">
         <v-row align="center">
-          <v-col cols="12" sm="8" class="d-flex align-center">
+          <v-col cols="12 " sm="8" class="d-flex align-center">
             <v-icon color="blue" class="mr-2">mdi-cog</v-icon>
             <div>
               <h3 class="blue--text text--darken-2 font-weight-bold mb-1">
@@ -23,7 +23,7 @@
     </v-card>
 
     <v-alert color="warning" dark icon="mdi-information" border="left" prominent outline>
-      <p>Apreciado(a) afiliado(a),</p>
+      <p>Apreciado(a) Administrador(a),</p>
       <p>
         En este módulo podrá gestionar la información administrativa de su empresa
         utilizada por el sistema para la generación de facturas electrónicas, reportes,
@@ -37,28 +37,65 @@
       </p>
     </v-alert>
 
-    <!-- <v-alert text type="info" class="mt-4">
-      <p>Apreciado(a) afiliado(a),</p>
-      <p class="">
-        Este módulo permite registrar y gestionar la información general de la empresa, la cual será utilizada en todos
-        los documentos oficiales del sistema, incluyendo facturas electrónicas, reportes, comprobantes y demás procesos
-        administrativos.
-      </p>
-    </v-alert> -->
-
     <v-alert dense text outlined color="primary" icon="mdi-text-box-multiple-outline" class="text-center">
-      <b>Configuracion De La Empresa</b>
+      <b>Configuración De La Empresa</b>
     </v-alert>
+
+    <v-card class="pa-4 mt-4" max-width="1300" outlined>
+      <v-form ref="formEmpresa" v-model="formValido">
+
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="empresa.razon_social" label="Razón Social" prepend-inner-icon="mdi-domain" dense
+              outlined required />
+          </v-col>
+
+          <v-col cols="12" md="12">
+            <v-text-field v-model="empresa.email" label="Email" prepend-inner-icon="mdi-map-marker" dense outlined
+              required />
+          </v-col>
+
+          <v-col cols="12" md="12">
+            <v-text-field v-model="empresa.direccion" label="Dirección" prepend-inner-icon="mdi-map-marker" dense
+              outlined required />
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <v-text-field v-model="empresa.nit" label="NIT" prepend-inner-icon="mdi-file-document" dense outlined required />
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <v-text-field v-model="empresa.telefono" label="Teléfono" prepend-inner-icon="mdi-phone" dense outlined
+              required />
+          </v-col>
+
+        </v-row>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" dark @click="guardarEmpresa">Guardar</v-btn>
+        </v-card-actions>
+
+      </v-form>
+    </v-card>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "general",
 
   data() {
-    return {}
+    return {
+      formValido: false,
+      empresa: {
+        razon_social: "",
+        email: "",
+        direccion: "",
+        nit: "",
+        telefono: "",
+      }
+    }
   }
 }
 </script>
